938a8bed7c609fc1fb6d8bae8a05cdaa982ff0a8
TakumaYoshioka <yoshioka@ttni.co.th>
2020-07-02 14:46:25 +0700

    Develop the first fuction for File converting

--- Program.cs
+++ Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
 
 namespace ChatbotTestFileconversion
 {
     class Program
     {
+        static string strCurrentDirectry = System.IO.Directory.GetCurrentDirectory();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputFileName;
+            string outputFileName;
+            string inputFileExtention;
+            string outputFileExtention;
+            
+            
+            try
+            {
+                inputFileName = args[0];
+                outputFileName = args[1];
+                inputFileExtention = args[2];
+                outputFileExtention = args[3];
+
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine(ex);
+                throw;
+            }
+
+            string inputSplitter = SelectSplitter(inputFileExtention);
+            string outputSplitter = SelectSplitter(outputFileExtention);
+            ConvertTableData(inputFileName, outputFileName, inputSplitter, outputSplitter);
+        }
+
+        static string SelectSplitter(string argFileExtention)
+        {
+            string splitter;
+            switch(argFileExtention)
+            {
+                case "csv":
+                    splitter = ",";
+                    break;
+                case "tsv":
+                    splitter = "\t";
+                    break;
+                default:
+                    throw new ArgumentException("File extention name is worng");
+            }
+            return splitter;
+        }
+        static void ConvertTableData(string argInputFileName, string argOutputFileName, string argInputSplitter, string argOutputSplitter)
+        {
+            List<string[]> contents = ReadTableFile(argInputFileName, argInputSplitter);
+            WriteTableFile(contents, argOutputFileName,argOutputSplitter);
+        }
+        static List<string[]> ReadTableFile(string argInputFileName, string argParserCharacter)
+        {
+            var tableContents = new List<string[]>();
+
+            try
+            {
+                StreamReader reader = new StreamReader(Path.Combine(strCurrentDirectry, argInputFileName), Encoding.GetEncoding("UTF-8"));
+                while (reader.Peek() >= 0) 
+                {
+                    string[] cols = reader.ReadLine().Split(argParserCharacter);
+                    tableContents.Add(cols);
+	            }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+                throw;
+            }
+            return tableContents;
+        }
+        static void WriteTableFile(List<string[]> argTableContents, string argOutputFileName, string argParserCharacter)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(argOutputFileName, false))
+                {
+                    foreach (var content in argTableContents)
+                    {
+                        sw.WriteLine(String.Join(argParserCharacter, content));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+                throw;
+            }
+
         }
     }
 }
